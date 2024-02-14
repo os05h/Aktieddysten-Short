0093c54581bd89af8cb2cc45e9de9dac17f09c81
os05h <52626254+os05h@users.noreply.github.com>
2024-02-14 19:00:35 +0000

    hm

--- run.py
+++ run.py
@@ -7,9 +7,10 @@ private = json.load(open('private.json'))
 #Login
 account = Aktiedysten_API(private['Username'], private['Password'], private['Game'])
 
-#Stocks
-Stocks = json.load(open('Stock,json'))
-for i in Stocks['Stocks']:
-    if(i['nr'] == 1):
-        Stock1 = i
-account.Buy(Stock1['MARKET'],Stock1['ITEM'],Stock1['MAX-Amount'],"STOCK")
+#Stocks Simpel buy
+#Stocks = json.load(open('Stock,json'))
+#for i in Stocks['Stocks']:
+#    if(i['nr'] == 1):
+#        Stock1 = i
+#account.Buy(Stock1['MARKET'],Stock1['ITEM'],Stock1['MAX-Amount'],"STOCK")
+
