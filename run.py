8fd0f068f885f987d8c26e82c93c5eb99d92d924
os05h <52626254+os05h@users.noreply.github.com>
2024-02-15 18:17:07 +0000

    working?

--- run.py
+++ run.py
@@ -15,8 +15,7 @@ for i in Stocks['Stocks']:
         Stock1 = i
 #account.Buy(Stock1['MARKET'],Stock1['ITEM'],Stock1['MAX-Amount'],"STOCK")
 curentpise = account.GetPrice(Stock1['MARKET'],Stock1['ITEM'], 10)
-print(curentpise)
-print(sum(curentpise)/len(curentpise))
+#print(f"Avage: {sum(curentpise)/len(curentpise)}  Prises: {curentpise}")
 bought = 0
 while(True):
 
@@ -24,26 +23,28 @@ while(True):
         break
     else:
         curentpise = account.GetPrice(Stock1['MARKET'],Stock1['ITEM'], 10)
-        print(curentpise)
-        print(sum(curentpise)/len(curentpise))
-        if sum(curentpise)/len(curentpise) < Stock1["Change"] and bought == 0:
+        print(f"Avage: {sum(curentpise)/len(curentpise)}  Prises: {curentpise}")
+        if not bought == 0:
+            print(f"selsfor profit: {curentpise[len(curentpise)-1]-bought}")
+        if sum(curentpise)/len(curentpise)-Stock1["Change"] > curentpise[len(curentpise)-1] and bought == 0:
             bought = curentpise[len(curentpise)-1]
             print("")
             print("Buy")
             print(curentpise[len(curentpise)-1])
             print("")
 
-        if sum(curentpise)/len(curentpise) > Stock1["Change"] and not bought == 0:
-            bought = 0
+        if curentpise[len(curentpise)-1]-bought > Stock1["Change"] and not bought == 0:
+            
             print("")
             print("Sell")
             print(curentpise[len(curentpise)-1])
-            print(f"Profit: {(bought-curentpise[len(curentpise)-1])}")
+            print(f"Profit: {curentpise[len(curentpise)-1]-bought}")
             print("")
             log = open("log.txt", "w")
-            log.write(f"Profit: {(bought-curentpise[len(curentpise)-1])}")
+            log.writelines(f"Profit: {curentpise[len(curentpise)-1]-bought}")
             log.close()
+            bought = 0
 
 
 
-    time.sleep(10)
+    time.sleep(20)
